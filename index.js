62fd87c4889a38403849ccc82230b84d0686c9b9
Nanda241998 <117671873+Nanda241998@users.noreply.github.com>
2022-11-24 23:37:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
+(function(){
+    let screen = document.querySelector('.screen')
+    let buttons= document.querySelectorAll('.btn')
+    let clear = document.querySelector('.btn-clear')
+    let equal= document.querySelector('.btn-equal')
 
-const express = require('express');
-const path = require('path');
+    buttons.forEach(function(button){
+        button.addEventListener('click',function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
 
-const app = express();
+        })
+    });
+equal.addEventListener('click',function(e){
+    if(screen.value == ''){
+        screen.value = "";
 
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+    }else{
+        let answer = eval(screen.value);
+        screen.value=answer;
+    }
+})
+equal.addEventListener('click',function(e){
+    screen.value="";
+})
+})();
